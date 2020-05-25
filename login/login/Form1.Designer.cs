0c6894c8de9b3f747fb82fc9f694c7734a2836bc
Yorandequay <yoranq@hotmail.com>
2020-05-25 14:17:54 +0200

    Can add accounts

---
+++ login/login/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace login
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbUsrname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbPassword = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // txbUsrname
+            // 
+            this.txbUsrname.Location = new System.Drawing.Point(16, 30);
+            this.txbUsrname.Name = "txbUsrname";
+            this.txbUsrname.Size = new System.Drawing.Size(100, 20);
+            this.txbUsrname.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // txbPassword
+            // 
+            this.txbPassword.Location = new System.Drawing.Point(16, 74);
+            this.txbPassword.Name = "txbPassword";
+            this.txbPassword.PasswordChar = '*';
+            this.txbPassword.Size = new System.Drawing.Size(100, 20);
+            this.txbPassword.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Create";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txbPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txbUsrname);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txbUsrname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbPassword;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ login/login/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using MySql.Data.MySqlClient;
+
+namespace login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            createAccount();
+        }
+
+        private void createAccount()
+        {
+            string username = txbUsrname.Text;
+            string password = txbPassword.Text;
+            string MySQLConnectionString = "datasource=localhost;port=3306;username=root;password=;database=logintest;";
+            string query = "INSERT INTO data(username, password)VALUES('" + username + "', '" + password + "');";
+            MySqlConnection databaseConnection = new MySqlConnection(MySQLConnectionString);
+            MySqlCommand cmdAdd = new MySqlCommand(query, databaseConnection);
+            MySqlDataReader myReader;
+
+            if (username == "" && password == "")
+            {
+                MessageBox.Show("Voer een gebruikersnaam en wachtwoord in.");
+            }
+
+
+
+            try
+            {
+                databaseConnection.Open();
+                myReader = cmdAdd.ExecuteReader();
+                MessageBox.Show("Uw account is toegevoegd.");
+                while(myReader.Read())
+                {
+
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Error: " + e.Message);
+            }
+        }
+    }
+}
