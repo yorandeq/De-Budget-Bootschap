07182d4bd800b23d1626e8c4d6117c0d05f73e55
Yorandequay <yoranq@hotmail.com>
2020-05-26 16:08:14 +0200

    classes toegevoegd

--- login/login/Form1.cs
+++ login/login/Form1.cs
@@ -14,47 +14,16 @@ namespace login
 {
     public partial class Form1 : Form
     {
+        connection con = new connection();
         public Form1()
         {
             InitializeComponent();
+            con.checkConn();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            createAccount();
-        }
-
-        private void createAccount()
-        {
-            string username = txbUsrname.Text;
-            string password = txbPassword.Text;
-            string MySQLConnectionString = "datasource=localhost;port=3306;username=root;password=;database=logintest;";
-            string query = "INSERT INTO data(username, password)VALUES('" + username + "', '" + password + "');";
-            MySqlConnection databaseConnection = new MySqlConnection(MySQLConnectionString);
-            MySqlCommand cmdAdd = new MySqlCommand(query, databaseConnection);
-            MySqlDataReader myReader;
-
-            if (username == "" && password == "")
-            {
-                MessageBox.Show("Voer een gebruikersnaam en wachtwoord in.");
-            }
-
-
-
-            try
-            {
-                databaseConnection.Open();
-                myReader = cmdAdd.ExecuteReader();
-                MessageBox.Show("Uw account is toegevoegd.");
-                while(myReader.Read())
-                {
-
-                }
-            }
-            catch(Exception e)
-            {
-                MessageBox.Show("Error: " + e.Message);
-            }
+            con.addAccount(txbUsrname.Text, txbPassword.Text);
         }
 
         private void label3_Click(object sender, EventArgs e)
---
+++ login/login/connection.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace login
+{
+    public class connection
+    {
+        public static string MySQLConnectionString = "datasource=localhost;port=3306;username=root;password=;database=logintest;";
+        MySqlConnection databaseConnection = new MySqlConnection(MySQLConnectionString);
+
+        public void checkConn()
+        {
+            try
+            {
+                databaseConnection = new MySqlConnection(MySQLConnectionString);
+                databaseConnection.Open();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error: " + e.Message);
+            }
+        }
+
+        public void addAccount(string username, string password)
+        {
+            if (username == "" && password == "")
+            {
+                MessageBox.Show("Voer een gebruikersnaam en wachtwoord in.");
+            } 
+            else
+            {
+                try
+                {
+                    string query = "INSERT INTO data(username, password)VALUES('" + username + "', '" + password + "');";
+                    MySqlCommand cmdAdd = new MySqlCommand(query, databaseConnection);
+                    cmdAdd.ExecuteReader();
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Error: " + e);
+                }
+                
+            }
+        }
+    }
+}
