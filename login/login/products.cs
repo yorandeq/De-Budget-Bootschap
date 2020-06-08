222b416e2866d99ad0c8db5aa88d5080e65a1e39
Roy de Graaf <roydegraaf8@hotmail.com>
2020-06-08 15:29:20 +0200

    Created seperate products and offers containers

--- login/login/products.cs
+++ login/login/products.cs
@@ -17,7 +17,8 @@ namespace login
         connection connection = new connection();
         DataLayer DataLayer = new DataLayer();
 
-        int Yposition = 1;
+        int YpositionProducts = 1;
+        int YpositionOffers = 1;
 
         public products()
         {
@@ -26,9 +27,9 @@ namespace login
 
         private void products_Load(object sender, EventArgs e)
         {
-            DataTable aa = DataLayer.Query("SELECT brand, category FROM discount_offers", p => { });
-            DataRow[] row = aa.Select();
-            for (int i = 0; i < row.Length; i++)
+            DataTable offers = DataLayer.Query("SELECT brand, category FROM discount_offers", p => { });
+            DataRow[] offersRow = offers.Select();
+            for (int i = 0; i < offersRow.Length; i++)
             {
                 //components
                 Panel itemContainer = new Panel();
@@ -38,8 +39,8 @@ namespace login
                 //options
                 itemContainer.BackColor = SystemColors.ControlDark;
                 itemContainer.Height = 100;
-                itemContainer.Width = 500;
-                itemContainer.Top = Yposition * 112;
+                itemContainer.Width = 300;
+                itemContainer.Top = YpositionOffers * 112;
                 itemContainer.Left = 12;
 
                 //itemImg.Image = Properties.Resources.albert_heijn;
@@ -49,26 +50,67 @@ namespace login
                 //itemImg.Width = 80;
                 //itemImg.Height = 80;
 
-                itemName.Text = row[i][0].ToString();
+                itemName.Text = offersRow[i][0].ToString();
                 itemName.Top = 10;
                 itemName.Left = 110;
 
-                itemBtn.Text = row[i][1].ToString();
+                itemBtn.Text = offersRow[i][1].ToString();
                 itemBtn.BackColor = SystemColors.Control;
                 itemBtn.Width = 80;
                 itemBtn.Top = 10;
-                itemBtn.Left = 410;
+                itemBtn.Left = 210;
 
                 //move next item down
-                Yposition++;
+                YpositionOffers++;
                 //add panel
                 this.Controls.Add(itemContainer);
                 //add controls inside panel
                 itemContainer.Controls.Add(itemImg);
                 itemContainer.Controls.Add(itemName);
                 itemContainer.Controls.Add(itemBtn);
+            }
 
-                //MessageBox.Show(row[i][1].ToString());
+            DataTable products = DataLayer.Query("SELECT name, total_price FROM discount_products", p => { });
+            DataRow[] productsRow = products.Select();
+            for (int i = 0; i < productsRow.Length; i++)
+            {
+                //components
+                Panel itemContainer = new Panel();
+                PictureBox itemImg = new PictureBox();
+                Label itemName = new Label();
+                Button itemBtn = new Button();
+                //options
+                itemContainer.BackColor = SystemColors.ControlDark;
+                itemContainer.Height = 100;
+                itemContainer.Width = 300;
+                itemContainer.Top = YpositionProducts * 112;
+                itemContainer.Left = 320;
+
+                //itemImg.Image = Properties.Resources.albert_heijn;
+                //itemImg.SizeMode = PictureBoxSizeMode.Zoom;
+                //itemImg.Top = 10;
+                //itemImg.Left = 10;
+                //itemImg.Width = 80;
+                //itemImg.Height = 80;
+
+                itemName.Text = productsRow[i][0].ToString();
+                itemName.Top = 10;
+                itemName.Left = 110;
+
+                itemBtn.Text = productsRow[i][1].ToString();
+                itemBtn.BackColor = SystemColors.Control;
+                itemBtn.Width = 80;
+                itemBtn.Top = 10;
+                itemBtn.Left = 210;
+
+                //move next item down
+                YpositionProducts++;
+                //add panel
+                this.Controls.Add(itemContainer);
+                //add controls inside panel
+                itemContainer.Controls.Add(itemImg);
+                itemContainer.Controls.Add(itemName);
+                itemContainer.Controls.Add(itemBtn);
             }
         }
     }
