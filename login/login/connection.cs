d1db2f3b0ef2efae7af26526b7641e855f46fa30
Leon van den Heuvel <leonheuveltje@gmail.com>
2020-06-19 14:43:51 +0200

    Prevented blance from going negative

--- login/login/connection.cs
+++ login/login/connection.cs
@@ -284,7 +284,11 @@ namespace login
                     int product_id = (int)productId;
                     int product_amount = 1;
                     float paid = (float)price;
-                    DataLayer.Query("INSERT INTO `registration` (`registration_id`, `user`, `product`, `product_amount`, `paid`) VALUES (NULL, @UserId, @ProductId, @ProductAmount, @TotalPrice)",
+                    bool transactionComplete = SubtractBalance(paid);
+
+                    if (transactionComplete)
+                    {
+                        DataLayer.Query("INSERT INTO `registration` (`registration_id`, `user`, `product`, `product_amount`, `paid`) VALUES (NULL, @UserId, @ProductId, @ProductAmount, @TotalPrice)",
                             p =>
                             {
                                 p.Add("@UserId", MySqlDbType.Int32, 255).Value = user_id;
@@ -292,8 +296,9 @@ namespace login
                                 p.Add("@ProductAmount", MySqlDbType.Int32, 255).Value = product_amount;
                                 p.Add("@TotalPrice", MySqlDbType.Float, 255).Value = paid;
                             });
-                    MessageBox.Show("Bedankt voor uw bestelling!");
-                    SubtractBalance(paid);
+                        MessageBox.Show("Bedankt voor uw bestelling!");
+                    }
+                    
                 } 
                 catch (Exception e)
                 {
@@ -330,7 +335,7 @@ namespace login
         }
 
         // Method for subtracting balance
-        public void SubtractBalance(float paid)
+        public bool SubtractBalance(float paid)
         {
             DataTable getBalance = DataLayer.Query("SELECT balance FROM `users` WHERE user_id = @UserID",
                 p =>
@@ -340,12 +345,19 @@ namespace login
 
             float newBalance = float.Parse(getBalance.Rows[0]["balance"].ToString()) - paid;
 
+            if (newBalance < 0)
+            {
+                MessageBox.Show("U heeft niet genoeg balans om deze aankoop te doen");
+                return false;
+            }
+
             DataTable setBalance = DataLayer.Query("UPDATE `users` SET balance = @Balance WHERE user_id = @UserId",
                 p =>
                 {
                     p.Add("@Balance", MySqlDbType.Decimal, 255).Value = newBalance;
                     p.Add("@UserID", MySqlDbType.Int16, 11).Value = GlobalMethods.LoginInfo.UserID;
                 });
+            return true;
         }
 
         // Method for deleting a specific user from the database
